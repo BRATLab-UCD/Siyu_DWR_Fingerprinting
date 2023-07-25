2922fd2cc8dde77ae34c19b393157428bb5d12c4
QiSiyu <syqi@ucdavis.edu>
2023-07-25 14:56:09 -0700

    Upload data and scripts
    Data and scripts updated. Instructions can be found in the ReadMe file.

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # Siyu_DWR_Fingerprinting
  Projects in collaboration with the California Department of Water Resources. Apply machine learning for X2 estimation and fingerprinting.
+ ## What's in this repo
+ * [FP_Stations.pdf](FP_Stations.pdf): the map showing 19 locations of interest.
+ * [Fingerprinting_Readings](Fingerprinting_Readings): Reading materials.
+   - [Ref2](Ref2_2018_DSM2_Emulation_Chen et al.pdf): the paper that proposes training one ANN per location per boundary source. The used input variables are (according to Table 4 in the paper):
+   <!-- <style type="text/css">
+   .tg  {border-collapse:collapse;border-spacing:0;}
+   .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+     overflow:hidden;padding:10px 5px;word-break:normal;}
+   .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+     font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+   .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+   </style> -->
+   <table class="tg">
+   <thead>
+     <tr>
+       <th class="tg-0pky">Number</th>
+       <th class="tg-0pky">Input variable</th>
+       <th class="tg-0pky">Units</th>
+     </tr>
+   </thead>
+   <tbody>
+     <tr>
+       <td colspan="3" class="tg-0pky">_Flow &amp; water level_</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">1</td>
+       <td class="tg-0pky">Sacramento River flow at Freeport</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">2</td>
+       <td class="tg-0pky">San Joaquin River flow at Vernalis</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">3</td>
+       <td class="tg-0pky">Mokelumne River flow</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">4</td>
+       <td class="tg-0pky">Cosumnes River flow</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">5</td>
+       <td class="tg-0pky">Calaveras River flow</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">6</td>
+       <td class="tg-0pky">Yolo Bypass flow</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">7</td>
+       <td class="tg-0pky">Delta island net channel depletions</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">8</td>
+       <td class="tg-0pky">Delta combined exports</td>
+       <td class="tg-0pky">cfs</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">9</td>
+       <td class="tg-0pky">Martinez tide</td>
+       <td class="tg-0pky">ft MSL</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky" colspan="3"><span style="font-style:italic">_Gates &amp; barrier operations_</span></td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">10</td>
+       <td class="tg-0pky">Grant Line Canal</td>
+       <td class="tg-0pky">0, 1</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">11</td>
+       <td class="tg-0pky">Middle River near Tracy</td>
+       <td class="tg-0pky">0, 1</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">12</td>
+       <td class="tg-0pky">Old River at Tracy</td>
+       <td class="tg-0pky">0, 1</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">13</td>
+       <td class="tg-0pky">Head of Old River</td>
+       <td class="tg-0pky">0, 1</td>
+     </tr>
+     <tr>
+       <td class="tg-0pky">14</td>
+       <td class="tg-0pky">Delta Cross Channel (DCC)</td>
+       <td class="tg-0pky">0, 1, 2\*</td>
+     </tr>
+   </tbody>
+   </table>
+   \* 0: Gate is fully closed; 1: one gate is fully open; 2: two gates are fully open.
+
+ * [Data](Data): all the output data. Note that the column names in each csv file represent one location of interest in the Delta, as marked in the [map](FP_Stations.pdf).
+    - Six csv files: each for one boundary source.
+    - [All.csv](Data/All.csv): summation of the six boundary sources.
+    - [dsm2_ann_inputs_historical_ec.xlsx](Data/dsm2_ann_inputs_historical_ec.xlsx): Input dataset, including input variables:
+          - #2, #8, #9, #14
+          - Northern flow = #1 + #3 + #4 + #5 + #6
+          - Delta consumptive usage (input #7 ?)
+          - San Joaquin River inflow salinity at Vernalis.
+
+ * [Train_Fingerprinting_ANN.ipynb](Train_Fingerprinting_ANN.ipynb): Colab training script, same architectures as Delta modelling ANNs. Outputs are flattened into an $114 \times 1$ vector.
+
+## A few thoughts:
+ * Output scaling: still linearly normalize each variable individually?
+ * Output arrangement: still treat them as a flattened vector?
+  - This project is about identifying the volume contributed by 6 sources at 19 locations, they should be internally correlated.
---
+++ preprocessing_utils.py
@@ -0,0 +1,182 @@
+# from sklearn import preprocessing
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
+
+# import tensorflow as tf
+from tensorflow import keras
+import joblib
+
+import pandas as pd
+import numpy as np
+
+def synchronize(dfx,dfy,lead_time=0,lead_freq='D'):
+    '''
+    synchronizes on index dfx and dfy and return tuple of synchronized data frames
+    Note: assumes dfy has only one column
+    '''
+    # if isinstance(dfx.columns, pd.MultiIndex):
+    #     pass
+    # else:
+    if lead_time > 0:
+        dfy.index = dfy.index.shift(-lead_time,freq=lead_freq)
+    dfsync=pd.concat([dfx,dfy],axis=1).dropna()
+    return dfsync.iloc[:,:-len(dfy.columns)],dfsync.iloc[:,-len(dfy.columns):]
+
+
+def create_antecedent_inputs(df,ndays=8,window_size=11,nwindows=10):
+    '''
+    create data frame for CALSIM ANN input
+    Each column of the input dataframe is appended by :-
+    * input from each day going back to 7 days (current day + 7 days) = 8 new columns for each input
+    * 11 day average input for 10 non-overlapping 11 day periods, starting from the 8th day = 10 new columns for each input
+
+    Returns
+    -------
+    A dataframe with input columns = (8 daily shifted and 10 average shifted) for each input column
+
+    '''
+    arr1=[df.shift(n) for n in range(ndays)]
+    dfr=df.rolling(str(window_size)+'D',min_periods=window_size).mean()
+    arr2=[dfr.shift(periods=(window_size*n+ndays),freq='D') for n in range(nwindows)]
+    df_x=pd.concat(arr1+arr2,axis=1).dropna()# nsamples, nfeatures
+    return df_x
+
+def split(df, calib_slice, valid_slice):
+    if type(calib_slice) == list:
+        calib_set = pd.concat([df[slc] for slc in calib_slice],axis=0)
+    else:
+        calib_set = df[calib_slice]
+    if type(valid_slice) == list:
+        valid_set = pd.concat([df[slc] for slc in valid_slice],axis=0)
+    else:
+        valid_set = df[valid_slice]
+    return calib_set, valid_set
+
+class myscaler():
+    def __init__(self):
+        self.min_val = float('inf')
+        self.max_val = -float('inf')
+    
+    def fit_transform(self, data, name=''):
+        if isinstance(data, list):
+            df = pd.concat(data,axis=0)
+            df[df==-2]=float('nan')
+            self.min_val = df.min()
+            self.max_val = df.max()
+        else:
+            # if incoming data is not a list of 2D dataframes, then it must be a dataframe with 3 dimensions
+            assert isinstance(data.columns, pd.MultiIndex),'Does not support %s data type' % name
+            self.min_val = data.min()
+            self.max_val = data.max()
+            
+    def update(self, other_scaler):
+        self.min_val = np.minimum(self.min_val,other_scaler.min_val)
+        self.max_val = np.maximum(self.max_val,other_scaler.max_val)
+    
+        
+    def transform(self, data):
+        return (data - self.min_val) * 1.0 / (self.max_val - self.min_val)
+    
+    def inverse_transform(self, data):
+        if type(data)==np.ndarray:
+            max_val = self.max_val.to_numpy().reshape(1,-1)
+            min_val = self.min_val.to_numpy().reshape(1,-1)
+            return data * (max_val - min_val) + min_val
+        else:
+            return data * (self.max_val - self.min_val) + self.min_val
+    
+def create_xyscaler(dfin,dfout):
+    # xscaler=MinMaxScaler()
+    xscaler=myscaler()
+    _ = xscaler.fit_transform(dfin,name='inputs')
+    #
+    yscaler=myscaler()
+    _ = yscaler.fit_transform(dfout,name='outputs')
+    return xscaler, yscaler
+
+def jitter(x, sigma=0.03):
+    # https://arxiv.org/pdf/1706.00527.pdf
+    return x + np.random.normal(loc=0., scale=sigma, size=x.shape)
+
+def dropout(x, p=0.03):
+    return x * (np.random.binomial([np.ones(x.shape)],
+                                  1-p)[0]) # * (1.0/(1-p)) # re-normalize the vector to compensate for 0's
+
+
+def apply_augmentation(x, apply_aug=False,noise_sigma=0.03,dropout_ratio=0):
+    np.random.seed(0)
+    if apply_aug:
+        x = jitter(x,sigma=noise_sigma)
+        x = dropout(x,p=dropout_ratio)
+    return x
+
+def create_training_sets(dfin, dfout, calib_slice=slice('1940','2015'), valid_slice=slice('1923','1939'),
+                         train_frac=None,
+                         ndays=8,window_size=11,nwindows=10,
+                         noise_sigma=0.,dropout_ratio=0.,
+                         lead_time=0,lead_freq='D',
+                         xscaler=None, yscaler=None,
+                         keep_months_only=None):
+    '''
+    dfin is a dataframe that has sample (rows/timesteps) x nfeatures 
+    dfout is a dataframe that has sample (rows/timesteps) x 1 label
+    Both these data frames are assumed to be indexed by time with daily timestep
+
+    This calls create_antecedent_inputs to create the CALSIM 3 way of creating antecedent information for each of the features
+
+    Returns a tuple of two pairs (tuples) of calibration and validation training set where each set consists of input and output
+    it also returns the xscaler and yscaler in addition to the two tuples above
+    '''
+    # create antecedent inputs aligned with outputs for each pair of dfin and dfout
+    dfina,dfouta=[],[]
+    # scale across all inputs and outputs
+    if (xscaler is None) or (yscaler is None):
+        xscaler,yscaler=create_xyscaler(dfin,dfout)
+    if keep_months_only is not None:
+        print('Training on %s seasons (%s) only' % (keep_months_only,'Jul. to Dec.' if keep_months_only == 'dry' else 'Jan. to Jun.'))
+    
+    for dfi,dfo in zip(dfin,dfout):
+        dfi,dfo=synchronize(dfi,dfo,lead_time=lead_time,lead_freq=lead_freq)
+        dfi,dfo=pd.DataFrame(xscaler.transform(dfi),dfi.index,columns=dfi.columns),pd.DataFrame(yscaler.transform(dfo),dfo.index,columns=dfo.columns)
+        dfi,dfo=synchronize(create_antecedent_inputs(dfi,ndays=ndays,window_size=window_size,nwindows=nwindows),dfo)
+        if keep_months_only == 'wet':
+            dfi = dfi[(dfi.index.month>=7) & (dfi.index.month<=12)]
+            dfo = dfo[(dfo.index.month>=7) & (dfo.index.month<=12)]
+        elif keep_months_only == 'dry':
+            dfi = dfi[(dfi.index.month>=1) & (dfi.index.month<=6)]
+            dfo = dfo[(dfo.index.month>=1) & (dfo.index.month<=6)]
+            
+        dfina.append(dfi)
+        dfouta.append(dfo)
+        
+    
+    # split in calibration and validation slices
+    if train_frac is None:
+        dfins=[split(dfx,calib_slice,valid_slice) for dfx in dfina]
+        dfouts=[split(dfy,calib_slice,valid_slice) for dfy in dfouta]
+    else:
+        train_sample_index = dfina[0].sample(frac=train_frac,random_state=0).index
+        dfins=[(dfx.loc[dfx.index.isin(train_sample_index)],dfx.loc[~dfx.index.isin(train_sample_index)]) for dfx in dfina]
+        dfouts=[(dfy.loc[dfy.index.isin(train_sample_index)],dfy.loc[~dfy.index.isin(train_sample_index)]) for dfy in dfouta]
+        print('Randomly selecting %d samples for training, %d for test' % (dfins[0][0].shape[0],dfins[0][1].shape[0]))
+
+    # append all calibration and validation slices across all input/output sets
+    xallc,xallv=dfins[0]
+    for xc,xv in dfins[1:]:
+        xallc=np.append(apply_augmentation(xallc,noise_sigma=noise_sigma,dropout_ratio=dropout_ratio),xc,axis=0)
+        xallv=np.append(xallv,xv,axis=0)
+    yallc, yallv = dfouts[0]
+    for yc,yv in dfouts[1:]:
+        yallc=np.append(yallc,yc,axis=0)
+        yallv=np.append(yallv,yv,axis=0)
+    return (xallc,yallc),(xallv,yallv),xscaler,yscaler
+
+
+def conv_filter_generator(ndays=7,window_size = 11, nwindows=10):
+    w = np.zeros((1,ndays+nwindows*window_size,ndays+nwindows))
+    for ii in range(ndays):
+        w[0,ndays+nwindows*window_size-ii-1,ii] = 1
+    for ii in range(nwindows):
+        w[0,((nwindows-ii-1)*window_size):((nwindows-ii)*window_size),ndays+ii] = 1/window_size
+    return w
